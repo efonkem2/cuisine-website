0ef569feab430f852e81384b6a44a0bd9f272dac
efonkem <argorithmictech@gmail.com>
2026-02-23 23:10:24 -0500

    server updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,12 +20,22 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-22.04-amd64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
 
